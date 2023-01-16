ae989499737ed7cba4abba73377f87edbc05e999
whynaidu <naiduvedant@gmail.com>
2023-01-16 18:50:24 +0530

    Image Upload in conventional Way

--- backend/api.js
+++ backend/api.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const multer = require('multer');
+const path = require('path');
+    
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})); 
 require('./db/conn')
@@ -14,10 +19,11 @@ const wallpaperschema = new mongoose.Schema({
         type: String,
         required:true
     },
-    wallaperimage: {
-        data:Buffer,
-        contentType:String
-
+    category: {
+        type: String,
+    },
+    wallpaper_url: {
+         type: String,
     }
 
 
@@ -32,39 +38,69 @@ const PORT = process.env.PORT
 
 const Storage = multer.diskStorage({
     destination: "uploads",
-    filenme: (req, file, cb) => {
-        cb(null, file.originalname);
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 });
 
 const upload = multer({
     storage:Storage
-}).single('wallpaper')
-
-app.post('/upload', (req, res) => {
-
-    upload(req, res, (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            const newwallpaper = new wallpaper({
-                name: req.body.name,
-                wallaperimage: {
-                    data: req.file.filename,
-                    contentType: "image/png"
-                },
-            });
-            newwallpaper.save()
-                .then(() => res.send("Uploaded Succesful"))
-                .catch(err => console.log(err))
-        }
-    })
 })
 
 
-app.get('/wallpaper', async (req, res) => {
+app.post("/upload", upload.single('wallpaper'), (req, res) => {
 
-    const data = await wallpaper.find();
+ 
+
+    const newwallpaper = new wallpaper({
+        name: req.body.name,
+        category: req.body.category,
+        wallpaper_url:`${req.file.filename}`
+    })
+    newwallpaper.save()
+        .then(() => res.send('uploaded'))
+    .catch(err => console.log(err))
+})
+
+
+
+
+
+// app.post('/upload', (req, res) => {
+
+//     upload(req, res, (err) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             const newwallpaper = new wallpaper({
+//                 name: req.body.name,
+//                 wallaperimage: {
+//                     data: req.file.filename,
+//                     contentType: "image/png"
+//                 },
+//             });
+//             newwallpaper.save()
+//                 .then(() => res.send("Uploaded Succesful"))
+//                 .catch(err => console.log(err))
+//         }
+//     })
+// })
+
+
+app.get('/wallpaper/:category', async (req, res) => {
+
+    
+    const data = await wallpaper.find({ category: req.params.category }, {});
+    console.log(data);
+    res.send(data);
+
+});
+
+
+
+app.post('/deleteall', async (req, res) => {
+
+    const data = await wallpaper.deleteMany();
     console.log(data);
     res.send(data);
 
