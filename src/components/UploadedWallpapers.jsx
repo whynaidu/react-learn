6396addb67350601867122d242f499f23a08a79e
whynaidu <naiduvedant@gmail.com>
2023-02-13 14:02:49 +0530

    made impovedments to upload UI

--- src/components/UploadedWallpapers.jsx
+++ src/components/UploadedWallpapers.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import StayPrimaryPortraitIcon from "@mui/icons-material/StayPrimaryPortrait";
 import ComputerIcon from "@mui/icons-material/Computer";
-import Desktop from "./Desktop";
+import ModalImage from "react-modal-image";
 import { Skeleton } from "@mui/material";
 import axios from "axios";
 
-import MobileImage from "./MobileImage";
-import DesktopImage from "./DesktopImage";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 export default class UploadedWallpapers extends Component {
   constructor() {
@@ -17,34 +16,48 @@ export default class UploadedWallpapers extends Component {
       desktopLength: "",
       mobileData: [],
       desktopData: [],
+      deleted: false,
+      status:""
     };
-    }
+  }
 
   componentDidMount() {
     const getMobile = async () => {
       const res = await axios.get("http://localhost:3001/mobile");
       const data = await res.data;
-      this.setState({mobileLength:data.length},() => console.log(this.state.mobileLength));
+      this.setState({ mobileLength: data.length }
+      );
       this.setState({ mobileData: data });
       this.setState({ image: true });
+      this.setState({ uploaded: "" }, () => console.log(this.state.uploaded));
+
     };
     getMobile();
     const getDesktop = async () => {
       const res = await axios.get("http://localhost:3001/");
-        const data = await res.data;
-              this.setState({ desktopLength: data.length }, () =>
-                console.log(this.state.desktopLength)
-              );
+      const data = await res.data;
+      this.setState({ desktopLength: data.length });
 
       this.setState({ desktopData: data });
       this.setState({ image: true });
+      this.setState({uploaded:""},() => console.log(this.state.uploaded))
     };
     getDesktop();
   }
 
+  deleteClick = (objid) => {
+    axios
+      .post(`http://localhost:3001/delete/${objid}`)
+      .then((res) => {
+        this.setState({ deleted: true }, () => console.log(this.state.deleted));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
 
   componentDidUpdate(prevProps, prevState) {
-    
     if (prevProps.status != this.props.status) {
       const getMobile = async () => {
         const res = await axios.get("http://localhost:3001/mobile");
@@ -57,8 +70,6 @@ export default class UploadedWallpapers extends Component {
       };
       getMobile();
 
-
-      
       const getDesktop = async () => {
         const res = await axios.get("http://localhost:3001/");
         const data = await res.data;
@@ -70,16 +81,41 @@ export default class UploadedWallpapers extends Component {
         this.setState({ image: true });
       };
       getDesktop();
+    }
+
+    if (prevState.deleted != this.state.deleted) {
+       const getMobile = async () => {
+         const res = await axios.get("http://localhost:3001/mobile");
+         const data = await res.data;
+         this.setState({ mobileLength: data.length }, () =>
+           console.log(this.state.mobileLength)
+         );
+         this.setState({ mobileData: data });
+         this.setState({ image: true });
+       };
+       getMobile();
+
+       const getDesktop = async () => {
+         const res = await axios.get("http://localhost:3001/");
+         const data = await res.data;
+         this.setState({ desktopLength: data.length }, () =>
+           console.log(this.state.desktopLength)
+         );
+
+         this.setState({ desktopData: data });
+         this.setState({ image: true });
+       };
+      getDesktop();
+      
+      this.setState({
+        deleted:false
+      })
       
     }
   }
+
   render() {
-                    {
-                      console.log(this.props.status);
-                    }
-
     return (
-
       <div>
         <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
           <ul
@@ -128,14 +164,29 @@ export default class UploadedWallpapers extends Component {
         </div>
         <div id="myTabContent">
           <div
-            className="hidden rounded-lg bg-gray-50 p-4 dark:bg-gray-800"
+            className="rounded-lg bg-gray-50 p-4 dark:bg-gray-800"
             id="desktop"
             role="tabpanel"
             aria-labelledby="profile-tab"
           >
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
               {this.state.desktopData.map((elem, key) => {
-                return <DesktopImage key={key} url={elem.wallpaper_url} />;
+                return (
+                  <div key={key}>
+                    <ModalImage
+                      className="h-64 w-full rounded-lg border-2 border-solid border-sky-500 object-cover"
+                      small={`../uploads/${elem.wallpaper_url}`}
+                      large={`../uploads/${elem.wallpaper_url}`}
+                      hideZoom={true}
+                    />
+                    <button
+                      className="relative left-[85%] top-[-17%] rounded-full border bg-white px-2 py-2 text-sm font-semibold text-red-600 hover:border-transparent hover:bg-red-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
+                      onClick={() => this.deleteClick(elem._id)}
+                    >
+                      <DeleteForeverIcon />
+                    </button>
+                  </div>
+                );
               })}
 
               {!this.state.image &&
@@ -158,7 +209,22 @@ export default class UploadedWallpapers extends Component {
           >
             <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
               {this.state.mobileData.map((elem, key) => {
-                return <MobileImage key={key} url={elem.wallpaper_url} />;
+                return (
+                  <div key={key}>
+                    <ModalImage
+                      className="h-[332.53px] w-full rounded-lg border-2 border-solid border-sky-500 object-cover md:h-[494px] lg:h-[759.81px]"
+                      small={`../uploads/${elem.wallpaper_url}`}
+                      large={`../uploads/${elem.wallpaper_url}`}
+                      hideZoom={true}
+                    />
+                    <button
+                      className="relative left-[85%] top-[-7%] rounded-full border bg-white px-2 py-2 text-sm font-semibold text-red-600 hover:border-transparent hover:bg-red-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
+                      onClick={() => this.deleteClick(elem._id)}
+                    >
+                      <DeleteForeverIcon />
+                    </button>
+                  </div>
+                );
               })}
               {!this.state.image &&
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12].map((key) => (
